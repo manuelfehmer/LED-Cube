a4cc6ddc2ab84a9740b34eb99060b8b8644af96d
Manuel Fehmer <manuelfehmer@gmail.com>
2013-06-08 14:33:39 +0200

    start

---
+++ DUO-LED-CUBE/DUO-LED-CUBE/DUO-LED-CUBE.c
@@ -0,0 +1,16 @@
+/*
+ * DUO_LED_CUBE.c
+ *
+ * Created: 08.06.2013 14:32:29
+ *  Author: Manuel
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
